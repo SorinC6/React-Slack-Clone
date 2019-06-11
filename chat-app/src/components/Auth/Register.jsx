06fb0fbfa434c0e3ab7cce5d8defa2fd1ab4beb8
Sorin Chis <kish.sorin@yahoo.com>
2019-06-11 14:06:38 +0300

    created Login and Refister component layout

---
+++ chat-app/src/components/Auth/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Register extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Register</p>
+      </div>
+    );
+  }
+}
+
+export default Register;
