4748109a089dbf1f52de311b5b1e3436589b423a
Sorin Chis <kish.sorin@yahoo.com>
2019-06-11 14:15:06 +0300

    added Route for Register and Login

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 
 const Root = () => (
   <Router>
     <Switch>
-      <Route path="/" component={App} />
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Switch>
   </Router>
 );
